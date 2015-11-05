84fab06897dab0fd8272bbaafc6cbdba128ea424
Paramvir Dehal <psdehal@lbl.gov>
2015-11-04 17:11:37 -0800

    interleaved support

--- lib/Trimmomatic/TrimmomaticImpl.py
+++ lib/Trimmomatic/TrimmomaticImpl.py
@@ -96,14 +96,22 @@ This sample module contains one small method - count_contigs.
             cmdProcess = subprocess.Popen(cmdstring, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             stdout, stderr = cmdProcess.communicate()
             report = "cmdstring: " + cmdstring + " stdout: " + stdout + " stderr: " + stderr
+
+            cmdstring = " ".join( (TrimmomaticCmd, 
+                            'forward.fastq', 
+                            'reverse.fastq',
+                            'forward_paired.fastq',  
+                            'forward_unpaired.fastq',
+                            'reverse_paired.fastq',
+                            'reverse_unpaired.fastq',
+                            TrimmomaticParams) )
         else:
             reverse_reads_file = open(reverse_reads['file_name'], 'w', 0)
             r = requests.get(reverse_reads['url']+'/node/'+reverse_reads['id']+'?download', stream=True, headers=headers)
             for chunk in r.iter_content(1024):
                 reverse_reads_file.write(chunk)
 
-        s = " "
-        cmdstring = s.join( (TrimmomaticCmd, 
+            cmdstring = " ".join( (TrimmomaticCmd, 
                             forward_reads['file_name'], 
                             reverse_reads['file_name'],
                             'forward_paired_'   +forward_reads['file_name'],
